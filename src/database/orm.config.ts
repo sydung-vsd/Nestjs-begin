4bd0ff1b1c990f14951f622ad93a24972e7f031e
Vũ Dũng <vudung@Vus-MacBook-Pro.local>
2023-08-22 08:43:44 +0700

    db

--- src/database/orm.config.ts
+++ src/database/orm.config.ts
@@ -7,7 +7,7 @@ import { UserAuthEntity } from "src/user.auth/entities/user.auth.entity";
 export const config: TypeOrmModuleOptions = {
     type: 'postgres',
     username: 'postgres',
-    password: '12345678',
+    password: 'dung12345678',
     host: 'localhost',
     port: 5432,
     database: 'nestjs-begin',
